87ecb107437f504c99835c7fddc0576620d4fba2
sokko <angelinasokol2005@gmail.com>
2024-11-15 01:59:04 +0700

    Исправила ошибку функции вывода правильных ответов

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -25,26 +25,28 @@ class MainActivity : AppCompatActivity() {
     private val answers = listOf(true, false, true, true)
     private var currentIndex = 0
     private var correctAnswerCount = 0
+
     companion object {
         private const val KEY_INDEX = "currentIndex"
         private const val KEY_SCORE = "correctAnswersCount"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         quiz = findViewById(R.id.quiz)
         btn_true = findViewById(R.id.btn_true)
         btn_false = findViewById(R.id.btn_false)
         btn_next = findViewById(R.id.btn_next)
-        updateQuestion()
+
         currentIndex = savedInstanceState?.getInt(KEY_INDEX) ?: 0
-        updateQuestion()
         correctAnswerCount = savedInstanceState?.getInt(KEY_SCORE) ?: 0
         updateQuestion()
 
-        btn_true.setOnClickListener {checkAnswer(true)}
-        btn_false.setOnClickListener {checkAnswer(false)}
+        btn_true.setOnClickListener { checkAnswer(true) }
+        btn_false.setOnClickListener { checkAnswer(false) }
         btn_next.setOnClickListener { nextQuestion() }
     }
 
@@ -53,30 +55,40 @@ class MainActivity : AppCompatActivity() {
         outState.putInt(KEY_INDEX, currentIndex)
         outState.putInt(KEY_SCORE, correctAnswerCount)
     }
-    private fun updateQuestion(){
+
+    private fun updateQuestion() {
         quiz.text = questions[currentIndex]
     }
-    private fun checkAnswer(userAnswer: Boolean){
+
+    private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = answers[currentIndex]
-        val message = if (userAnswer == correctAnswer) "Правильно!" else "Неправильно!"
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        if (userAnswer == correctAnswer) {
+            correctAnswerCount++  // Увеличиваем счётчик правильных ответов
+            Toast.makeText(this, "Правильно!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Неправильно!", Toast.LENGTH_SHORT).show()
+        }
+
         btn_true.visibility = View.INVISIBLE
         btn_false.visibility = View.INVISIBLE
+
         if (currentIndex == questions.size - 1) {
             btn_next.visibility = View.INVISIBLE
             btn_next.isEnabled = false
             showResult()
         }
     }
+
     private fun nextQuestion() {
         if (currentIndex < questions.size - 1) {
-            currentIndex = (currentIndex + 1) % questions.size
+            currentIndex++
             updateQuestion()
 
             btn_true.visibility = View.VISIBLE
             btn_false.visibility = View.VISIBLE
         }
     }
+
     private fun showResult() {
         Toast.makeText(
             this,
