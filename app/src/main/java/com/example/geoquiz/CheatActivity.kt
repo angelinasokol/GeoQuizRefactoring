2b113cb6968ba947f9aa9200f832a7079038f567
sokko <angelinasokol2005@gmail.com>
2024-11-18 10:52:58 +0700

    Оформила страницу подсказки, добавила работоспособность

--- app/src/main/java/com/example/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -1,17 +1,28 @@
 package com.example.geoquiz
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.geoquiz.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textView = TextView(this)
-        textView.textSize = 20f
-        textView.setPadding(16, 16, 16, 16)
-        textView.text = "CheatActivity"
-        setContentView(textView)
+
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val answerIsTrue = intent.getBooleanExtra("EXTRA_ANSWER_IS_TRUE", false)
+
+        binding.btnShowAnswer.setOnClickListener {
+            val answerText = if (answerIsTrue) "Правда" else "Ложь"
+            binding.txtAnswer.text = answerText
+            val resultIntent = Intent()
+            resultIntent.putExtra("EXTRA_ANSWER_SHOWN", true)
+            setResult(RESULT_OK, resultIntent)
+        }
     }
 }
